68a715056607c90f27bc11d9c328fbf1241f5c20
qq53 <vap0r@vap0r.cn>
2015-10-06 05:34:06 +0800

    sths

--- make.py
+++ make.py
@@ -1,19 +1,33 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 from jinja2 import Environment, FileSystemLoader
 import os
+import codecs
 
 env = Environment(loader = FileSystemLoader('templates'))
 template = env.get_template('index.html')
 ds = []
 ds.append({
-	'title':'1',
-	'contents':'2'
+	'title':'加密算法总结',
+	'tags':('tag1','tag2'),
+        'year':2015
 	})
 ds.append({
-	'title':'3',
-	'contents':'4'
+	'title':'WinHex 14.1 SR-6 爆破',
+	'tags':('tag1',)
+	})
+ds.append({
+	'title':'西普学院 – 逆向 – 此处无声',
+	'tags':('tag1','tag2'),
+        'year':2014
 	})
 
-with open('tmp.html','wt') as f:
-	f.write(template.render(posts=ds))
+css_list = ('sidebar',)
+
+u = 'http://blog.vap0r.cn'
+
+with codecs.open('templates\\tmp.html', 'w', 'utf-8') as f:
+    f.write(template.render(arts=ds, css=css_list, index=u))
+	
+exit()
