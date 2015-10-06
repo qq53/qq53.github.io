42482e01406a7ec03bfe71ef22c923abcb7c8eac
qq53 <vap0r@vap0r.cn>
2015-10-07 04:52:57 +0800

    more ths

--- art.py
+++ art.py
@@ -1,2 +1,60 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# author: vap0r
+# github: github.com/qq53
+
+from markdown import markdown
+import json
+import codecs
+import time
+from datetime import datetime
+import os
+
+def checkArtData(data):
+	try:
+		data['title'] and data['tags'] and data['content']
+	except:
+		print('Error article data format, must have ["title", "tags", "content"]')
+		return False
+	else:
+		return True
+
+def writeArt(data):
+	if checkArtData(data) == False:
+		return
+	fn = 'arts/' + str(int(time.time())) + '.json'
+	jdata = json.dumps(data)
+	with codecs.open(fn, 'w', encoding='utf-8') as f:
+		f.write(jdata)
+	
+def loadArt(fn):
+	try:
+		with codecs.open('arts/' + fn + '.json', 'r', encoding='utf-8') as f:
+			data = f.read()
+			d = json.loads(data)
+			if checkArtData(d) == False:
+				return None			
+			d['content'] = markdown(d['content'], output_format='HTML5')
+			return d
+	except:
+		print('read file ' + fn + ' error')
+
+def scan():
+	files = os.listdir('arts')
+	files = sorted(files)
+	
+	now = ''
+	lt = []
+	for f in files:
+		n = f[:f.find('.')]
+		t = datetime.fromtimestamp(int(n)).strftime('%Y')
+		d = loadArt(n)
+		if not d:
+			return None
+		if now != t:
+			now = t
+			d['year'] = now
+		lt.append(d)
+	return lt
+
+print(scan())
--- make.py
+++ make.py
@@ -1,37 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# author: vap0r
+# github: github.com/qq53
 
 from jinja2 import Environment, FileSystemLoader
 import os
 import codecs
-import markdown
+from art import scan
 
 env = Environment(loader = FileSystemLoader('templates'))
 template = env.get_template('index.html')
-ds = []
-ds.append({
-	'title':'加密算法总结',
-	'tags':('tag1','tag2'),
-        'year':2015
-	})
-ds.append({
-	'title':'WinHex 14.1 SR-6 爆破',
-	'tags':('tag1',)
-	})
-ds.append({
-	'title':'西普学院 – 逆向 – 此处无声',
-	'tags':('tag1','tag2'),
-        'year':2014
-	})
+
 
 css_list = ('sidebar',)
 
 u = 'http://blog.vap0r.cn'
 
 with codecs.open('templates\\tmp.html', 'w', 'utf-8') as f:
-    f.write(template.render(arts=ds, css=css_list, index=u))
-
-html = markdown.markdown('#title')
-print(html)
+    f.write(template.render(arts=scan(), css=css_list, index=u))
 	
 exit()
